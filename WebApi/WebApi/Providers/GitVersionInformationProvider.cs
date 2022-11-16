6de6f34a4e086c5d4019473c733860bf21c2eb82
Emil Nowak <emilento@gmail.com>
2022-11-16 23:21:51 +0100

    Demo

---
+++ WebApi/WebApi/Providers/GitVersionInformationProvider.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+
+namespace WebApi.Providers;
+
+public static class GitVersionInformationProvider
+{
+    private static readonly Assembly? Assembly = Assembly.GetEntryAssembly();
+
+    private static readonly Type? GitVersionInformationType = Assembly?.GetType("GitVersionInformation");
+
+    public static string Copyright => Assembly?.GetCustomAttribute<AssemblyCopyrightAttribute>()?.Copyright ?? string.Empty;
+
+    public static string FullSemVer => GetFieldValue("FullSemVer") ?? string.Empty;
+
+    public static string ShortSha => GetFieldValue("ShortSha") ?? string.Empty;
+
+    public static string Sha => GetFieldValue("Sha") ?? string.Empty;
+
+    private static string? GetFieldValue(string name) => GitVersionInformationType?.GetField(name)?.GetValue(null)?.ToString();
+}
